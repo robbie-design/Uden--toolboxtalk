1549afd2fc38470888c1a36206b0fc17cee0f254
robbie-design <robbie@udenbuilders.co.nz>
2026-03-19 10:43:07 +1300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,9 @@
 
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Uden Toolbox App is Working ✅</h1>
+      <p>If you see this, everything is set up correctly.</p>
+    </div>
+  );
+}
